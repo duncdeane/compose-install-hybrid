a29ca4a62be86d69fb6f1d7b1c0dbeb4657fdd23
Duncan Deane <duncan.deane@konghq.com>
2022-04-28 13:54:38 +0100

    Development

---
+++ check-uuid.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#!/bin/bash
+uuid="7632f5ab-4bac-11e6-bcb7-0cc47a6c4dbd"
+if [[ $uuid =~ ^\{?[A-F0-9a-f]{8}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{4}-[A-F0-9a-f]{12}\}?$ ]]; then
+    echo "true"
+else
+    echo "false"
+fi
